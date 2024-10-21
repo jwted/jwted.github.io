35cc3d8400fa92a9ace7722be87a595e80c93177
jwted <92721496+jwted@users.noreply.github.com>
2024-10-21 14:34:03 +0100

    work section halfway done

--- my-portfolio/src/main.jsx
+++ my-portfolio/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import Navbar from './components/Navbar/Navbar'
 import Info from './components/Info/Info'
 import Hero from './components/Sections/Hero'
+import Work from './components/Sections/Work'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
@@ -10,5 +11,6 @@ createRoot(document.getElementById('root')).render(
       <Navbar />
       <Hero />
       <Info/>
+      <Work/>
   </StrictMode>,
 )
