ff540f3c02547835c997badfb70e70990e0a6343
jwted <92721496+jwted@users.noreply.github.com>
2025-01-17 20:02:20 +0000

    Navbar responsivity changes

--- my-portfolio/src/components/Navbar/Navbar.jsx
+++ my-portfolio/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,34 @@
 import "./NavbarCss.css";
 import Variants from '../menu2.jsx'
 
+
+import {
+  Avatar,
+  Dropdown,
+  DropdownDivider,
+  DropdownHeader,
+  DropdownItem,
+  Navbar,
+  NavbarBrand,
+  NavbarCollapse,
+  NavbarLink,
+  NavbarToggle,
+} from "flowbite-react";
+
+
+
+
 import animationData from '../../assets/animation/menu.json'
 // import Lottie from 'react-lottie'
 
 import { useState } from "react";
 
-function Navbar() {
+function NavbarFunc() {
+
+
+
+
+  
   /* const [isAnimating, setIsAnimating] = useState(false);
   const defaultOptions = {
     loop:false,
@@ -34,21 +56,20 @@ function Navbar() {
 
   return (
     
-      <div className="flex h-nav justify-between items-center fixed px-10 w-full" id="navbar">
-        <h2 className="name ">João Pais</h2>
-        <div className=" hidden tablet:inline px-2 py-1 box">
-          <button className="px-2 max-h-6 btn_trans ">Info</button>
-          <button className="px-2 max-h-6 btn_trans">Work</button>
-        </div>
-        <Variants />
-        {/* <Variants className="tablet:hidden pc:hidden" /> */}
-        
+    <div className="flex tablet:justify-between h-nav justify-between items-center fixed px-10 w-full" id="navbar">
+  <h2 className="name">João Pais</h2>
+  
+  <div className="hidden tablet:inline px-2 py-1 box">
+    <button className="px-2 max-h-6 btn_trans">Info</button>
+    <button className="px-2 max-h-6 btn_trans">Work</button>
+  </div>
 
-
-        <h4 className="hidden pc:inline">Resume</h4>
-      </div>
-    
+  <div className="flex  gap-6 items-center">
+    <h4 className="hidden pc:inline cursor-pointer">LinkedIn</h4>
+    <h4 className="hidden pc:inline cursor-pointer">Resume</h4>
+  </div>
+</div>
   );
 }
 
-export default Navbar;
+export default NavbarFunc;
--- my-portfolio/tailwind.config.js
+++ my-portfolio/tailwind.config.js
@@ -1,8 +1,11 @@
+
+const flowbite = require("flowbite-react/tailwind");
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    flowbite.content(),
   ],
   theme: {
     screens:{
@@ -16,7 +19,9 @@ export default {
       }
     },
   },
-  plugins: [],
+  plugins: [
+    flowbite.plugin(),
+  ],
 }
 
 
